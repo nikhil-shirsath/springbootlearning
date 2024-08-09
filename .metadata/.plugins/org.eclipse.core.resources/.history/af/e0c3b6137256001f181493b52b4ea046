5ac445169af481f20f7f82499e533a96065b2455
nikhil shirsath <001nikhilshirsath001@gmail.com>
2024-08-09 22:51:55 +0530

    qualifier annotation

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/af/e0c3b6137256001f181493b52b4ea046
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+
+	private String lName;
+	private int lRam;
+	private int lSsd;
+	public String getlName() {
+		return lName;
+	}
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+	public int getlRam() {
+		return lRam;
+	}
+	public void setlRam(int lRam) {
+		this.lRam = lRam;
+	}
+	public int getlSsd() {
+		return lSsd;
+	}
+	public void setlSsd(int lSsd) {
+		this.lSsd = lSsd;
+	}
+	
+	public void compile() {
+		System.out.println("compilation of the code is going on ");
+	}
+	
+	@Override
+	public String toString() {
+		return "Laptop [lName=" + lName + ", lRam=" + lRam + ", lSsd=" + lSsd + "]";
+	}
+	
+	
+}
--- secondproject/src/main/java/com/example/demo/Alian.java
+++ secondproject/src/main/java/com/example/demo/Alian.java
@@ -1,10 +1,12 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component //creates object in a spring container //to create object in spring container we uses component annotation 
+//otherwise it gives Null ponter exeption
 @Scope(value="prototype") //using this scope you will create this object multiple times.
 // using this scope annottion it will not create a default object for us untile we calls getBean(class name 
 
@@ -18,6 +20,7 @@ public class Alian {
 	// For that autowired is comes in the picture
 	@Autowired //this tells the alian class that we have a componenet of named laptop go and search for it. 
 	//If we not uses this autowired keyword then our code gets null pointer exeption. 
+	@Qualifier("lap1") // this searches component based on the name given in it // to use it we should also give name to the component 
 	private Laptop laptop;
 	
 	
